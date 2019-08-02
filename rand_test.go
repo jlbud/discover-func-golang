2a8308219ec0c42d5824c498a94fab89eac3039d
kevin <wei.liu@ifchange.com>
2019-08-02 16:11:55 +0800

    add rand

---
+++ rand_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestRand(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+	questionIds := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
+	perm := rand.Perm(len(questionIds))
+
+	fmt.Println(perm[:5])
+}
