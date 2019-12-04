1b5b4899818f5a1aa5b5e3dd064db2116973845d
kevin <wei.liu@ifchange.com>
2019-12-04 19:30:55 +0800

    add TestSyncMutex

---
+++ sync_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+var l sync.Mutex
+
+func TestSyncMutex(t *testing.T) {
+	a := 0
+	p_a := &a
+	go func() {
+		l.Lock()
+		fmt.Println("3")
+		time.Sleep(5 * time.Second)
+		*p_a += 1
+		fmt.Println("4")
+		l.Unlock()
+	}()
+
+	go func() {
+		l.Lock()
+		fmt.Println("1")
+		*p_a += 1
+		fmt.Println("2")
+		l.Unlock()
+	}()
+
+	go func() {
+		l.Lock()
+		time.Sleep(3 * time.Second)
+		fmt.Println("5")
+		*p_a += 1
+		fmt.Println("6")
+		l.Unlock()
+	}()
+	fmt.Println(*p_a)
+
+	time.Sleep(15 * time.Second)
+}
