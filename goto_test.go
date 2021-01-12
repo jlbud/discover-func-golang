9dee214f2be032d3662d291edcdb9b6dcbd95ce7
kevin <wei.liu@ifchange.com>
2021-01-12 10:43:04 +0800

    update goto, '使用goto break最外层for循环'

--- goto_test.go
+++ goto_test.go
@@ -9,14 +9,29 @@ func Test_goto(t *testing.T) {
 	count := 0
 retry:
 	fmt.Println("a")
-	count ++
+	count++
 
 	if count == 100 {
 		return
 	}
 	fmt.Println("c")
 
-	for i := 0; i < 100; i ++ {
+	for i := 0; i < 100; i++ {
 		goto retry
 	}
+}
+
+// 使用goto break最外层for循环
+func TestGoto1(t *testing.T) {
+	i := 0
+f:
+	for true {
+		i++
+		t.Logf("i: %d", i)
+		if i == 10 {
+			t.Logf("i is at %d break", i)
+			break f
+		}
+	}
+	t.Log("end")
 }
