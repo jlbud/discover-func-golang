27ec2602039a652272bef4c308edc8683a7877ce
kevin <wei.liu@ifchange.com>
2021-02-02 14:42:22 +0800

    [http_server_test.go] add HTTP中间件

--- http_server_test.go
+++ http_server_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 )
 
 // https请求去掉证书验证
@@ -22,4 +23,34 @@ func TestHttpServer(t *testing.T) {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body))
+}
+
+// HTTP中间件
+func TestHttpMiddleware(t *testing.T) {
+	http.Handle("/hello", timeMiddleWare(http.HandlerFunc(middleWareHello)))
+	http.Handle("/world", timeMiddleWare(http.HandlerFunc(middleWareWorld)))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func middleWareHello(wr http.ResponseWriter, r *http.Request) {
+	_, _ = wr.Write([]byte("hello"))
+}
+
+func middleWareWorld(wr http.ResponseWriter, r *http.Request) {
+	_, _ = wr.Write([]byte("world"))
+}
+
+// 中间件通过包装handler再返回一个新的handler
+func timeMiddleWare(next http.Handler) http.Handler {
+	// TODO 没明白
+	return http.HandlerFunc(func(wr http.ResponseWriter, r *http.Request) {
+		// 中间件做了打印时间的事情
+		timS := time.Now()
+		next.ServeHTTP(wr, r)
+		timeE := time.Since(timS)
+		fmt.Println(timeE)
+	})
 }
