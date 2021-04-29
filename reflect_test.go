9096043540b105ad76085a38b42545a205c7e7b9
kevin <wei.liu@ifchange.com>
2021-04-29 11:21:22 +0800

    add reflect_test test case

---
+++ reflect_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestGetFieldName(t *testing.T) {
+	n := struct {
+		Name string
+		Age  string
+	}{}
+	strArr, _ := GetFieldName(n)
+	t.Log(strArr)
+}
+
+func TestGetFieldTagName(t *testing.T) {
+	n := struct {
+		Name string `json:"name" db:"localhost"`
+		Age  string `json:"age" db:"localhost"`
+	}{}
+	strArr, _ := GetTagName(n)
+	t.Log(strArr)
+}
+
+// Gets the name of the field in the struct
+func GetFieldName(structName interface{}) ([]string, error) {
+	t := reflect.TypeOf(structName)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	} else if t.Kind() != reflect.Struct {
+		return nil, errors.New("not struct error, please check type")
+	}
+	fieldNum := t.NumField()
+	result := make([]string, 0, fieldNum)
+	for i := 0; i < fieldNum; i++ {
+		result = append(result, t.Field(i).Name)
+	}
+	return result, nil
+}
+
+// Gets the value of the Tag in the struct, or returns the field value if there is no Tag
+func GetTagName(structName interface{}) ([]string, error) {
+	t := reflect.TypeOf(structName)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	} else if t.Kind() != reflect.Struct {
+		return nil, errors.New("not struct error, please check type")
+	}
+	fieldNum := t.NumField()
+	result := make([]string, 0, fieldNum)
+	for i := 0; i < fieldNum; i++ {
+		tagName := t.Field(i).Name
+		tags := strings.Split(string(t.Field(i).Tag), "\"")
+		if len(tags) > 1 {
+			// get first tag value
+			tagName = tags[1]
+		}
+		result = append(result, tagName)
+	}
+	return result, nil
+}
