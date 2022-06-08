3ec82d409c4690941bf6d93b3754f8ea539ef1fc
kevin <liuwei_Kevin@unisound.com>
2022-06-08 09:19:38 +0800

    Added unicode test

---
+++ unicode_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+	"unicode"
+)
+
+func StrToUnicode(str string) string {
+	DD := []rune(str)
+	finallStr := ""
+	for i := 0; i < len(DD); i++ {
+		if unicode.Is(unicode.Scripts["Han"], DD[i]) {
+			textQuoted := strconv.QuoteToASCII(string(DD[i]))
+			finallStr += textQuoted[1 : len(textQuoted)-1]
+		} else {
+			h := fmt.Sprintf("%x", DD[i])
+			finallStr += "\\u" + isFullFour(h)
+		}
+	}
+	return finallStr
+}
+
+func isFullFour(str string) string {
+	if len(str) == 1 {
+		str = "000" + str
+	} else if len(str) == 2 {
+		str = "00" + str
+	} else if len(str) == 3 {
+		str = "0" + str
+	}
+	return str
+}
+
+////////// string to unicode
+func TestStr2Unicode(t *testing.T) {
+	s := StrToUnicode(">")
+	fmt.Println(s)
+}
+
+func unicode2utf8(source string) string {
+	var res = []string{""}
+	sUnicode := strings.Split(source, "\\u")
+	var context = ""
+	for _, v := range sUnicode {
+		var additional = ""
+		if len(v) < 1 {
+			continue
+		}
+		if len(v) > 4 {
+			rs := []rune(v)
+			v = string(rs[:4])
+			additional = string(rs[4:])
+		}
+		temp, err := strconv.ParseInt(v, 16, 32)
+		if err != nil {
+			context += v
+		}
+		context += fmt.Sprintf("%c", temp)
+		context += additional
+	}
+	res = append(res, context)
+	return strings.Join(res, "")
+}
+
+////////// unicode to utf8
+func TestUnicode2utf8(t *testing.T) {
+	unicodeStr := "\u003c" // >
+	s := unicode2utf8(unicodeStr)
+	fmt.Println(s)
+}
