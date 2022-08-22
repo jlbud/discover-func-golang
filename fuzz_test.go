1aabf3e45f4fa1433d517b4d49b2fadef569eeca
kevin <liuwei_Kevin@unisound.com>
2022-08-22 17:11:45 +0800

    Optimize fuzz testing code

--- fuzz_test.go
+++ fuzz_test.go
@@ -1,14 +1,22 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-// go test -fuzz=Fuzz -fuzztime 10s -run FuzzEqual
-func FuzzEqual(t *testing.F) {
-	t.Add([]byte{1, 2, 3}, []byte{1, 2, 3})
-	t.Fuzz(func(t *testing.T, a []byte, b []byte) {
+// go test -v -fuzz=Fuzz -fuzztime 10s -run FuzzEqual // auto input fuzz params,
+// has " matches more than one fuzz test" bug.
+// go test -v -run FuzzEqual // only input two where Add func() special cases.
+func FuzzEqual(f *testing.F) {
+	f.Add([]byte{1, 2, 3}, []byte{1, 2, 3})
+	f.Add([]byte{2, 3, 4}, []byte{1, 2, 3})
+	f.Fuzz(func(t *testing.T, a []byte, b []byte) {
 		bol := equalByte(a, b)
 		if bol {
-			t.Log("equal")
+			fmt.Println("equal")
+		}else{
+			fmt.Println("not equal")
 		}
 	})
 }
